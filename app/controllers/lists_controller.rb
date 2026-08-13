9f81ba976e4ed2b13352f89c9c6f0598a1ecb40a
GoneTarou <konnpen@gmail.com>
2026-08-13 19:22:43 +0900

    外部サイトのURLフォームを追加

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -34,6 +34,7 @@ class ListsController < ApplicationController
         :event_occurrence_id,
         { list_items_attributes: [ [
           :space_number,
+          :source_type,
           :source_url,
           :is_featured,
           :is_adult_content
