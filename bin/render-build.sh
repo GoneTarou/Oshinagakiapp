2ec776b7d5f580c3dbb2bb23281cd067282f1432
GoneTarou <konnpen@gmail.com>
2026-08-14 20:52:53 +0900

    renderbuildにmigrateとseedを追加

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,4 +2,6 @@
 set -o errexit
 
 bundle install
+RAILS_ENV=production bundle exec rails db:migrate
+RAILS_ENV=production bundle exec rails db:seed
 RAILS_ENV=production bundle exec rails assets:precompile
