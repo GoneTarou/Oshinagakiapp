e0ca6fcab85b186e736dcc48ebcabdf3eaab6f4c
GoneTarou <konnpen@gmail.com>
2026-08-17 02:21:28 +0900

    renderbuildを変更

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,6 @@
 set -o errexit
 
 bundle install
-RAILS_ENV=production bundle exec rails db:migrate
+RAILS_ENV=production bundle exec rails db:prepare
 RAILS_ENV=production bundle exec rails db:seed
 RAILS_ENV=production bundle exec rails assets:precompile
