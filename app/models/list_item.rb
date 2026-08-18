f638c011b141fdd2af76abd8c922e447963e14ab
GoneTarou <konnpen@gmail.com>
2026-08-18 18:33:37 +0900

    フォームのサークル情報をメモに変更　バリデーションも削除

--- app/models/list_item.rb
+++ app/models/list_item.rb
@@ -5,9 +5,7 @@ class ListItem < ApplicationRecord
 
   before_validation :normalize_attributes
 
-  validates :space_number,
-            uniqueness: { scope: :list_id, allow_nil: true },
-            length: { maximum: 100, allow_nil: true }
+  validates :space_number, length: { maximum: 30, allow_nil: true }
   validates :source_url, length: { maximum: 2048, allow_nil: true }
   validate :source_url_is_allowed
 
--- test/models/list_test.rb
+++ test/models/list_test.rb
@@ -7,4 +7,12 @@ class ListTest < ActiveSupport::TestCase
 
     assert list.valid?
   end
+
+  test "allows duplicate memos in a list" do
+    list = List.new(event: events(:one))
+    list.list_items.build(space_number: "8/18")
+    list.list_items.build(space_number: "8/18")
+
+    assert list.valid?
+  end
 end
