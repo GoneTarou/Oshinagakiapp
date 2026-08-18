9f4b6c96ad4ee1ca8d7eb4e58c95dab18ed63ff7
GoneTarou <konnpen@gmail.com>
2026-08-18 18:05:27 +0900

    更新情報を更新

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -6,6 +6,9 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
 
     assert_response :success
     assert_select "h1", text: "更新情報"
+    assert_select "time[datetime='2026-08-18']", count: 2
+    assert_select "h2", text: "使い方チュートリアルを追加しました"
+    assert_select "h2", text: "既存のリストを活用できるようになりました"
     assert_select "time[datetime='2026-08-17']"
     assert_select "a", text: "巡回リストを作成する", count: 0
   end
