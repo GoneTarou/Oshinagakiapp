d3ee0254cd0353edb6f7ae1a420099a6ae597609
GoneTarou <konnpen@gmail.com>
2026-08-19 23:10:00 +0900

    リスト作成で自動スクロール追加

--- test/system/list_drafts_test.rb
+++ test/system/list_drafts_test.rb
@@ -35,6 +35,28 @@ class ListDraftsTest < ApplicationSystemTestCase
     assert_field "list_list_items_attributes_0_space_number", with: ""
   end
 
+  test "scrolls to the first source URL only when an event is selected" do
+    page.execute_script(<<~JAVASCRIPT)
+      window.eventScrollCalls = []
+      Element.prototype.scrollIntoView = function(options) {
+        window.eventScrollCalls.push({ id: this.id, options: options })
+      }
+    JAVASCRIPT
+
+    select events(:one).name, from: "list_event_id"
+
+    scroll_calls = page.evaluate_script("window.eventScrollCalls")
+    assert_equal 1, scroll_calls.length
+    assert_equal "list_list_items_attributes_0_source_url", scroll_calls.first["id"]
+    assert_equal "smooth", scroll_calls.first.dig("options", "behavior")
+    assert_equal "center", scroll_calls.first.dig("options", "block")
+
+    page.execute_script("window.eventScrollCalls = []")
+    select "イベントを選択してください", from: "list_event_id"
+
+    assert_empty page.evaluate_script("window.eventScrollCalls")
+  end
+
   test "copies an existing list into the new list form" do
     source_list = List.create!(
       event: events(:one),
